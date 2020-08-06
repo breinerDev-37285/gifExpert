f192e5f2345ef0675ad0d80fcb577d903ad36493
breiner <jbpaig@gmail.com>
2020-08-06 17:49:24 -0500

    se agrega el docs

---
+++ docs/precache-manifest.70a7967b3404a16910dc50d8b0862878.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879884ca26e0d41e98f57a9c02d31b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f52a33259d149f770fb",
+    "url": "/static/css/2.48fd4033.chunk.css"
+  },
+  {
+    "revision": "e86a2d3890f8a75a02dc",
+    "url": "/static/css/main.c00dca6e.chunk.css"
+  },
+  {
+    "revision": "9f52a33259d149f770fb",
+    "url": "/static/js/2.a0fef117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0fef117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86a2d3890f8a75a02dc",
+    "url": "/static/js/main.c9d19041.chunk.js"
+  },
+  {
+    "revision": "8fbd23898c71323fe1aa",
+    "url": "/static/js/runtime-main.4c8be365.js"
+  },
+  {
+    "revision": "4b96047e4af086277cdaeb9e60857534",
+    "url": "/static/media/Ubuntu-Italic.4b96047e.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  }
+]);
